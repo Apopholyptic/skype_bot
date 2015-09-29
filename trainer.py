9bed3348fb4416147edc273e085fff362af83b9d
Apopholyptic <eye.conoclastastik@gmail.com>
2015-09-29 09:28:50 -0700

    Update trainer.py
    I'm the best

--- trainer.py
+++ trainer.py
@@ -38,6 +38,29 @@ def TrainDefault(botName):
         'you are padula bot',
         'No, im padula!',
         'No you\'re not',
-        'yes i am'])
+        'yes i am',
+        'I like to eat poop',
+        'Now, this is a story all about how',
+        'My life got flipped-turned upside down',
+        'And I'd like to take a minute',
+        'Just sit right there',
+        'I'll tell you how I became the prince of a town called Bel Air'
+        'In west Philadelphia born and raised',
+        'On the playground was where I spent most of my days,'
+        'Chillin' out maxin' relaxin' all cool',
+        'And all shootin some b-ball outside of the school',
+        'When a couple of guys who were up to no good',
+        'Started making trouble in my neighborhood',
+        'I got in one little fight and my mom got scared',
+        'She said You're movin' with your auntie and uncle in Bel Air',
+        'I whistled for a cab and when it came near',
+        'The license plate said fresh and it had dice in the mirror',
+        'If anything I could say that this cab was rare',
+        'But I thought Nah, forget it - Yo, holmes to Bel Air',
+        'I pulled up to the house about 7 or 8',
+        'And I yelled to the cabbie Yo holmes smell ya later',
+        'I looked at my kingdom',
+        'I was finally there',
+        'To sit on my throne as the Prince of Bel Air'])
 
 
